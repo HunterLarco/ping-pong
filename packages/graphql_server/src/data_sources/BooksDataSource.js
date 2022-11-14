f91a28a8020efb643193c57b469d8451721226a7
Hunter Larco <hunter@larcolabs.com>
2022-11-14 12:23:10 -0500

    extract BooksDataSource

---
+++ packages/graphql_server/src/data_sources/BooksDataSource.js
@@ -0,0 +1,66 @@
+import DataLoader from 'dataloader';
+import FuzzySearch from 'fuzzy-search';
+
+// In-memory data
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+    branch: 'ABRHS',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+    branch: 'NYPL',
+  },
+];
+
+// Data source
+
+export default class BooksDataSource {
+  batchQueryByBranch = new DataLoader(async (branches) => {
+    const results = {};
+    for (const branch of branches) {
+      results[branch] = [];
+    }
+
+    for (const book of books) {
+      if (!!results[book.branch]) {
+        results[book.branch].push(book);
+      }
+    }
+
+    return branches.map((branch) => results[branch]);
+  });
+
+  async queryByBranch(branch) {
+    return this.batchQueryByBranch.load(branch);
+  }
+
+  async fuzzySearch({ title, author, branch }) {
+    let index;
+    let results = books;
+
+    if (title) {
+      index = new FuzzySearch(results, ['title']);
+      results = index.search(title);
+    }
+
+    if (author) {
+      index = new FuzzySearch(results, ['author']);
+      results = index.search(author);
+    }
+
+    if (branch) {
+      index = new FuzzySearch(results, ['branch']);
+      results = index.search(branch);
+    }
+
+    return results;
+  }
+
+  async loadAll() {
+    return books;
+  }
+}
--- packages/graphql_server/src/index.js
+++ packages/graphql_server/src/index.js
@@ -1,63 +1,20 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import FuzzySearch from 'fuzzy-search';
-import DataLoader from 'dataloader';
 
 import schema from './schema.graphql';
+import BooksDataSource from './data_sources/BooksDataSource.js';
 
 const libraries = [{ branch: 'ABRHS' }, { branch: 'NYPL' }];
-
-class BooksDataSource {
-  constructor() {
-    this.books_ = [
-      {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-        branch: 'ABRHS',
-      },
-      {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-        branch: 'NYPL',
-      },
-    ];
-
-    this.index_ = new FuzzySearch(this.books_, ['title', 'author', 'branch']);
-  }
-
-  batchQuery = new DataLoader(async (terms) => {
-    return terms.map((term) => this.index_.search(term));
-  });
-
-  async queryByBranch(name) {
-    return this.books_.filter((book) => book.branch == name);
-  }
-
-  async query(term) {
-    return this.batchQuery.load(term);
-  }
-
-  async all() {
-    return this.books_;
-  }
-}
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
     async books(_, args, { dataSources }) {
-      if (!args.title && !args.author) {
-        return await dataSources.Books.all();
-      }
-
-      const byTitle = args.title
-        ? await dataSources.Books.query(args.title)
-        : [];
-      const byAuthor = args.author
-        ? await dataSources.Books.query(args.author)
-        : [];
-      return [...new Set([...byTitle, ...byAuthor])];
+      return await dataSources.Books.fuzzySearch({
+        title: args.title,
+        author: args.author,
+      });
     },
 
     libraries: () => libraries,
@@ -70,18 +27,12 @@ const resolvers = {
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
 });
 
 (async () => {
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  //  1. creates an Express app
-  //  2. installs your ApolloServer instance as middleware
-  //  3. prepares your app to handle incoming requests
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
     context: () => ({
