f65cb11504b9f3feee5ad394480b54aef3f2af65
Hunter Larco <hunter@larcolabs.com>
2022-11-15 18:24:47 -0500

    cleanup unused packages

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -2,11 +2,10 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import graphql from "@rollup/plugin-graphql";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), graphql()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
