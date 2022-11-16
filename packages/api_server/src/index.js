b91ceb589beb30c8c4d039e47c4375aa908356db
Hunter Larco <hunter@larcolabs.com>
2022-11-15 22:46:13 -0500

    use express middleware

--- packages/api_server/src/index.js
+++ packages/api_server/src/index.js
@@ -1,5 +1,12 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
 import { mergeResolvers } from '@graphql-tools/merge';
 
 import BookResolvers from '@/resolvers/Book/index.js';
@@ -11,21 +18,33 @@ import LibrarySchema from '@/schema/LibraryService.graphql';
 import BooksDataSource from '@/data_sources/BooksDataSource.js';
 import LibrariesDataSource from '@/data_sources/LibrariesDataSource.js';
 
+const app = express();
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
   typeDefs: [BookSchema, LibrarySchema],
   resolvers: mergeResolvers([LibraryResolvers, BookResolvers]),
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
 (async () => {
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: () => ({
-      dataSources: {
-        Books: new BooksDataSource(),
-        Libraries: new LibrariesDataSource(),
-      },
-    }),
-  });
+  await server.start();
 
-  console.log(`🚀 Server listening at: ${url}`);
+  app.use(
+    '/graphql',
+    cors(),
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: () => ({
+        dataSources: {
+          Books: new BooksDataSource(),
+          Libraries: new LibrariesDataSource(),
+        },
+      }),
+    })
+  );
+
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+  console.log(`🚀 Server listening at: localhost:4000`);
 })();
