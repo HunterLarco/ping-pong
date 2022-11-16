0ed4e7f6db954f3e1868733b6bde324074e2c273
Hunter Larco <hunter@larcolabs.com>
2022-11-15 20:22:18 -0500

    remove unused files

--- packages/frontend/src/views/Bar.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import { books } from './data.js';
-</script>
-
-<template>
-  <div># of books: {{ books ? books.books.length : 0 }}</div>
-</template>
--- packages/frontend/src/views/Foo.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import { books } from './data.js';
-</script>
-
-<template>
-  <div style="white-space: pre">{{ JSON.stringify(books, null, 2) }}</div>
-</template>
