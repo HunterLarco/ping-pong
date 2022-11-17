55322dee46d3c07c41d4c2fb28d1fa329e4a5dff
Hunter Larco <hunter@larcolabs.com>
2022-11-17 11:09:06 -0500

    remove MTG Treachery support

--- packages/api_server/src/context.js
+++ packages/api_server/src/context.js
@@ -1,13 +1,11 @@
 import BooksDataSource from '@/data_sources/BooksDataSource.js';
 import LibrariesDataSource from '@/data_sources/LibrariesDataSource.js';
-import MTGTreacheryDataSource from '@/data_sources/MTGTreacheryDataSource.js';
 
 export function createContext() {
   return {
     dataSources: {
       Books: new BooksDataSource(),
       Libraries: new LibrariesDataSource(),
-      MTGTreachery: MTGTreacheryDataSource,
     },
   };
 }
--- packages/api_server/src/schema.js
+++ packages/api_server/src/schema.js
@@ -3,17 +3,14 @@ import { mergeResolvers } from '@graphql-tools/merge';
 
 import BookResolvers from '@/resolvers/Book/index.js';
 import LibraryResolvers from '@/resolvers/Library/index.js';
-import AbilityResolvers from '@/resolvers/Ability/index.js';
 
 import BookSchema from '@/schema/BookService.graphql';
 import LibrarySchema from '@/schema/LibraryService.graphql';
-import AbilitySchema from '@/schema/AbilityService.graphql';
 
 export default makeExecutableSchema({
-  typeDefs: [BookSchema, LibrarySchema, AbilitySchema],
+  typeDefs: [BookSchema, LibrarySchema],
   resolvers: mergeResolvers([
     LibraryResolvers,
     BookResolvers,
-    AbilityResolvers,
   ]),
 });
