baab2ecb535367ffd3a0a8439748a4a01ee2c5a0
Hunter Larco <hunter@larcolabs.com>
2022-11-21 17:10:25 -0500

    join game scaffolding

--- packages/api_server/src/data_sources/PrismaDataSource.ts
+++ packages/api_server/src/data_sources/PrismaDataSource.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
+const kDuration_Days = 24 * 60 * 60 * 1000;
+
 export async function initiatePrismaClient() {
   const prisma = new PrismaClient();
   await prisma.$connect();
@@ -15,8 +17,28 @@ export class PrismaDataSource {
 
   async createGame() {
     return await this.#prismaClient.game.create({
+      data: {},
+    });
+  }
+
+  async createTemporaryUser(options: { name: string }) {
+    return await this.#prismaClient.temporaryUser.create({
       data: {
-        dateCreated: new Date(),
+        name: options.name,
+        dateExpires: new Date(Date.now() + kDuration_Days * 7),
+      },
+    });
+  }
+
+  async joinGame(options: { gameId: string; temporaryUserId: string }) {
+    await this.#prismaClient.game.update({
+      where: {
+        id: options.gameId,
+      },
+      data: {
+        playerIds: {
+          push: [options.temporaryUserId],
+        },
       },
     });
   }
--- packages/api_server/src/resolvers/Game/index.ts
+++ packages/api_server/src/resolvers/Game/index.ts
@@ -6,6 +6,19 @@ const GameResolvers: Resolvers = {
       const game = await dataSources.Prisma.createGame();
       return game;
     },
+
+    async joinGame(_0, { request }, { dataSources }) {
+      const user = await dataSources.Prisma.createTemporaryUser({
+        name: request.name,
+      });
+
+      await dataSources.Prisma.joinGame({
+        gameId: request.gameId,
+        temporaryUserId: user.id,
+      });
+
+      return user;
+    },
   },
 };
 
