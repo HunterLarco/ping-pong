a80c7e46a3a081d74a03d7660845a44cb858e0b6
Hunter Larco <hunter@larcolabs.com>
2022-11-29 09:19:49 -0500

    generate subgraphs

--- packages/api_server/codegen.ts
+++ packages/api_server/codegen.ts
@@ -1,12 +1,45 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: './graphql/**/*.graphql',
   generates: {
     './generated/graphql/ast.ts': {
+      schema: './graphql/**/*.graphql',
       plugins: ['@ping-pong/generate_graphql_ast'],
     },
     './generated/graphql/resolvers.ts': {
+      schema: './graphql/**/*.graphql',
+      plugins: ['typescript', 'typescript-resolvers'],
+      config: {
+        contextType: '@/RequestContext#RequestContext',
+        defaultMapper: 'Partial<{T}>',
+      },
+    },
+    './generated/graphql/game_service/resolvers.ts': {
+      schema: './graphql/game_service/**/*.graphql',
+      plugins: ['typescript', 'typescript-resolvers'],
+      config: {
+        contextType: '@/RequestContext#RequestContext',
+        defaultMapper: 'Partial<{T}>',
+      },
+    },
+    './generated/graphql/user_service/resolvers.ts': {
+      schema: './graphql/user_service/**/*.graphql',
+      plugins: ['typescript', 'typescript-resolvers'],
+      config: {
+        contextType: '@/RequestContext#RequestContext',
+        defaultMapper: 'Partial<{T}>',
+      },
+    },
+    './generated/graphql/identity_card_service/resolvers.ts': {
+      schema: './graphql/identity_card_service/**/*.graphql',
+      plugins: ['typescript', 'typescript-resolvers'],
+      config: {
+        contextType: '@/RequestContext#RequestContext',
+        defaultMapper: 'Partial<{T}>',
+      },
+    },
+    './generated/graphql/verification_service/resolvers.ts': {
+      schema: './graphql/verification_service/**/*.graphql',
       plugins: ['typescript', 'typescript-resolvers'],
       config: {
         contextType: '@/RequestContext#RequestContext',
