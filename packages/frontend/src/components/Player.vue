654f79a55534e07d36afa63e963643660850ce5c
Hunter Larco <hunter@larcolabs.com>
2022-12-04 21:53:42 -0800

    player state colors

--- packages/frontend/src/components/Player.vue
+++ packages/frontend/src/components/Player.vue
@@ -1,15 +1,37 @@
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
   data: {
     type: Object,
     required: true,
   },
 });
+
+const playerState = computed(() => {
+  switch (props.data.state) {
+    case 'Active':
+      return { color: 'gray', label: 'Playing' };
+    case 'Inactive':
+      return { color: 'red', label: 'Conceded' };
+    case 'Lost':
+      return { color: 'red', label: 'Lost' };
+    case 'Won':
+      return { color: 'green', label: 'Won' };
+  }
+});
 </script>
 
 <template>
   <div class="Player">
-    <div class="Name">{{ data.user.name }}</div>
+    <div class="Header">
+      <div class="Name">{{ data.user.name }}</div>
+      <div class="State">
+        <div :style="`background: ${playerState.color};`">
+          {{ playerState.label }}
+        </div>
+      </div>
+    </div>
     <div class="Identity">
       <img
         class="IdentityImage"
@@ -18,7 +40,6 @@ const props = defineProps({
       />
       <div class="IdentityPlaceholder" v-else />
     </div>
-    <div class="State">{{ data.state }}</div>
   </div>
 </template>
 
@@ -28,16 +49,33 @@ const props = defineProps({
 .Player {
   @include layout-vertical;
 
-  border-radius: 4px;
   text-align: center;
 }
 
-.State {
+.Header {
+  @include layout-horizontal;
+
   flex-shrink: 0;
 }
 
 .Name {
-  flex-shrink: 0;
+  flex-grow: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.State {
+  @include layout-center;
+
+  div {
+    background: gray;
+    border-radius: 2px;
+    flex-shrink: 0;
+    font-size: 9px;
+    font-weight: 700;
+    padding: 0 2px;
+    text-transform: uppercase;
+  }
 }
 
 .Identity {
