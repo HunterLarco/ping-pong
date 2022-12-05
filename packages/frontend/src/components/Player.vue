606bbda5854939a414774f178e7cbd2ed569163a
Hunter Larco <hunter@larcolabs.com>
2022-12-05 15:42:21 -0800

    health tile

--- packages/frontend/src/components/Player.vue
+++ packages/frontend/src/components/Player.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 
 const props = defineProps({
   data: {
@@ -8,37 +8,19 @@ const props = defineProps({
   },
 });
 
-const playerState = computed(() => {
-  switch (props.data.state) {
-    case 'Active':
-      return { color: 'gray', label: 'Playing' };
-    case 'Inactive':
-      return { color: 'red', label: 'Conceded' };
-    case 'Lost':
-      return { color: 'red', label: 'Lost' };
-    case 'Won':
-      return { color: 'green', label: 'Won' };
-  }
-});
+// TODO: move color to the backend
+const color = ref('#2196f3');
+
+const healthTileStyles = computed(() => ({
+  'background-color': color.value,
+}));
 </script>
 
 <template>
   <div class="Player">
-    <div class="Header">
+    <div class="HealthTile" :style="healthTileStyles">
+      <div class="HealthText">40</div>
       <div class="Name">{{ data.user.name }}</div>
-      <div class="State">
-        <div :style="`background: ${playerState.color};`">
-          {{ playerState.label }}
-        </div>
-      </div>
-    </div>
-    <div class="Identity">
-      <img
-        class="IdentityImage"
-        v-if="data.identity"
-        :src="data.identity.image"
-      />
-      <div class="IdentityPlaceholder" v-else />
     </div>
   </div>
 </template>
@@ -47,48 +29,49 @@ const playerState = computed(() => {
 @import '@/styles/layout';
 
 .Player {
-  @include layout-vertical;
-
-  text-align: center;
-}
-
-.Header {
-  @include layout-horizontal;
-
-  flex-shrink: 0;
-}
-
-.Name {
-  flex-grow: 1;
+  height: 300px;
   overflow: hidden;
-  text-overflow: ellipsis;
+  user-select: none;
+  width: 300px;
 }
 
-.State {
+.HealthTile {
+  @include layout-fill;
   @include layout-center;
 
-  div {
-    background: gray;
+  overflow: hidden;
+
+  &::before {
+    background: rgba(#000, 0.2);
     border-radius: 2px;
-    flex-shrink: 0;
-    font-size: 9px;
-    font-weight: 700;
-    padding: 0 2px;
-    text-transform: uppercase;
+    content: '';
+    display: block;
+    height: 4px;
+    left: 0;
+    margin: 0 auto;
+    mix-blend-mode: overlay;
+    position: absolute;
+    right: 0;
+    top: 6px;
+    width: 50px;
   }
 }
 
-.Identity {
-  flex-grow: 1;
-  overflow: hidden;
+.HealthText {
+  color: rgb(#000, 0.9);
+  font-size: 100px;
+  font-weight: 700;
 }
 
-.IdentityImage {
-  height: 400px;
-}
-
-.IdentityPlaceholder {
-  background: rgba(#000, 20%);
-  height: 400px;
+.Name {
+  bottom: 0;
+  color: rgb(#000, 0.2);
+  font-size: 60px;
+  left: 50%;
+  line-height: 70px;
+  mix-blend-mode: overlay;
+  position: absolute;
+  text-align: center;
+  transform: translateX(-50%);
 }
 </style>
