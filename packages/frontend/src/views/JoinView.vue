d4bfe2ab2cf0ddb414219468c3c8d1f6f3835cae
Hunter Larco <hunter@larcolabs.com>
2022-12-05 09:56:39 -0800

    add join page

---
+++ packages/frontend/src/views/JoinView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useMutation } from '@vue/apollo-composable';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    {{ route.params.gameId }}
+  </div>
+</template>
