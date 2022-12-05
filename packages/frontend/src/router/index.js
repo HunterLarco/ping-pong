47d4047bd9addd574b0ad01e1c425a09a5b90d66
Hunter Larco <hunter@larcolabs.com>
2022-12-04 20:09:19 -0800

    login page

--- packages/frontend/src/router/index.js
+++ packages/frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +11,19 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
   ],
+});
+
+router.beforeEach(async (to) => {
+  const authToken = localStorage.getItem('authorization');
+  if (!authToken && to.path != '/login') {
+    return '/login';
+  }
 });
 
 export default router;
---
+++ packages/frontend/src/views/LoginView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { watch } from 'vue';
+import { useQuery, useMutation, useSubscription } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
+
+const { mutate: mutation_issuePhoneVerification } = useMutation(
+  gql`
+  mutation IssuePhoneVerification($request: IssuePhoneVerificationRequest!) {
+    issuePhoneVerification(request: $request) {
+      knownPhoneNumber
+    }
+  }
+  `
+);
+
+function issuePhoneVerification(phoneNumber) {
+  mutation_issuePhoneVerification({ request: { phoneNumber } });
+}
+</script>
+
+<template>
+  <div>
+    <button @click="issuePhoneVerification('+100')">
+      Signup
+    </button>
+  </div>
+</template>
