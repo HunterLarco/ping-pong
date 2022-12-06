336230bfa51d4e099761fa6c1a99601654c8b6a2
Hunter Larco <hunter@larcolabs.com>
2022-12-05 17:12:36 -0800

    drag interaction

---
+++ packages/frontend/src/interactions/drag.js
@@ -0,0 +1,93 @@
+import { ref, watch } from 'vue';
+
+export function useDrag(elementRef) {
+  const initialX = ref(0);
+  const initialY = ref(0);
+  const deltaX = ref(0);
+  const deltaY = ref(0);
+  const velocityX = ref(0);
+  const velocityY = ref(0);
+
+  let history = [];
+  let onDragStart = () => {};
+  let onDragEnd = () => {};
+
+  function createCallbackArgs() {
+    return {
+      initialX: initialX.value,
+      initialY: initialY.value,
+      deltaX: deltaX.value,
+      deltaY: deltaY.value,
+      velocityX: velocityX.value,
+      velocityY: velocityY.value,
+    };
+  }
+
+  function onMouseDown(event) {
+    initialX.value = event.screenX;
+    initialY.value = event.screenY;
+    deltaX.value = 0;
+    deltaY.value = 0;
+    history = [];
+    onDragStart(createCallbackArgs());
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
+  }
+
+  function onMouseMove(event) {
+    deltaX.value = event.screenX - initialX.value;
+    deltaY.value = event.screenY - initialY.value;
+
+    history.push({ x: event.screenX, y: event.screenY, timestamp: Date.now() });
+    while (history[0].timestamp < Date.now() - 500) {
+      history.shift();
+    }
+    if (history.length > 1) {
+      const recent = history[history.length - 1];
+      const past = history[0];
+      velocityX.value =
+        (recent.x - past.x) / (recent.timestamp - past.timestamp);
+      velocityY.value =
+        (recent.y - past.y) / (recent.timestamp - past.timestamp);
+    }
+  }
+
+  function onMouseUp(event) {
+    onDragEnd(createCallbackArgs());
+    velocityX.value = 0;
+    velocityY.value = 0;
+    window.removeEventListener('mousemove', onMouseMove);
+    window.removeEventListener('mouseup', onMouseUp);
+  }
+
+  watch(elementRef, (element, previousElement) => {
+    if (previousElement) {
+      previousElement.removeEventListener('mousedown', onMouseDown);
+      window.removeEventListener('mousemove', onMouseMove);
+      window.removeEventListener('mouseup', onMouseUp);
+      initialX.value = 0;
+      initialY.value = 0;
+      deltaX.value = 0;
+      deltaY.value = 0;
+      velocityX.value = 0;
+      velocityY.value = 0;
+    }
+
+    element.addEventListener('mousedown', onMouseDown);
+  });
+
+  return {
+    initialX,
+    initialY,
+    deltaX,
+    deltaY,
+    velocityX,
+    velocityY,
+    onDragStart(callback) {
+      onDragStart = callback;
+    },
+    onDragEnd(callback) {
+      onDragEnd = callback;
+    },
+  };
+}
