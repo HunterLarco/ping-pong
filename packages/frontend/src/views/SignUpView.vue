4e64d04c5ec3c045151cc7deb23f906c5f42815c
Hunter Larco <hunter@larcolabs.com>
2023-01-03 09:42:35 -0500

    signin logic

--- packages/frontend/src/views/SignUpView.vue
+++ packages/frontend/src/views/SignUpView.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { useIssuePhoneVerificationMutation } from '@generated/graphql/operations';
+import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useToast } from 'vue-toastification';
 
 import CountryCodeDropdown from '@/components/CountryCodeDropdown.vue';
 import InputFrame from '@/components/InputFrame.vue';
@@ -7,7 +10,55 @@ import InputGroup from '@/components/InputGroup.vue';
 import MenuButton from '@/components/MenuButton.vue';
 import NavBar from '@/components/NavBar.vue';
 
+const router = useRouter();
+const toast = useToast();
+
+/// Form State
+
 const loading = ref(false);
+
+const screenName = ref('');
+const countryCode = ref('1' /* Default value is United States (+1) */);
+const localPhoneNumber = ref('');
+const phoneNumber = computed(
+  () => `+${countryCode.value}${localPhoneNumber.value}`
+);
+
+/// Actions
+
+function submit() {
+  loading.value = true;
+
+  const { mutate, onDone, onError } = useIssuePhoneVerificationMutation({
+    variables: {
+      phoneNumber: phoneNumber.value,
+    },
+  });
+
+  onError((error) => {
+    loading.value = false;
+    toast.error(error.message);
+    console.error('Failed to issue phone verification.', error);
+  });
+
+  onDone((result) => {
+    loading.value = false;
+    if (result?.data?.issuePhoneVerification.knownPhoneNumber) {
+      toast.error('User already exists.');
+      return;
+    }
+
+    router.push({
+      path: '/signup/otp',
+      query: {
+        name: screenName.value,
+        phoneNumber: phoneNumber.value,
+      },
+    });
+  });
+
+  mutate();
+}
 </script>
 
 <template>
@@ -19,19 +70,36 @@ const loading = ref(false);
       <div class="Form">
         <InputGroup label="Your Name">
           <InputFrame label="Screen Name" :disabled="loading">
-            <input type="text" :disabled="loading" />
+            <input
+              type="text"
+              :value="screenName"
+              @input="screenName = (<HTMLInputElement>$event.target).value"
+              :disabled="loading"
+            />
           </InputFrame>
         </InputGroup>
         <div class="InputGroupSpacer" />
         <InputGroup label="Your Phone Number">
           <InputFrame label="Country / Region" :disabled="loading">
-            <CountryCodeDropdown :disabled="loading" />
+            <CountryCodeDropdown v-model="countryCode" :disabled="loading" />
           </InputFrame>
           <InputFrame label="Phone Number" :disabled="loading">
-            <input type="tel" :disabled="loading" />
+            <input
+              type="tel"
+              :value="localPhoneNumber"
+              @input="
+                localPhoneNumber = (<HTMLInputElement>$event.target).value
+              "
+              :disabled="loading"
+            />
           </InputFrame>
         </InputGroup>
-        <MenuButton class="SubmitButton" text="Next" :disabled="loading" />
+        <MenuButton
+          class="SubmitButton"
+          text="Next"
+          @click="submit"
+          :disabled="loading || !screenName || !localPhoneNumber"
+        />
       </div>
     </div>
   </div>
