a4c7d9a6dc9ea81b4336e947d57c3a2a30b50557
Hunter Larco <hunter@larcolabs.com>
2023-01-03 22:27:58 -0500

    current game button

--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { useHomePageQuery } from '@generated/graphql/operations';
 import { useRouter } from 'vue-router';
 
 import MenuButton from '@/components/MenuButton.vue';
 import NavBar from '@/components/NavBar.vue';
 
 const router = useRouter();
+
+const { result: homePageResult } = useHomePageQuery();
 
 /// Actions
 
@@ -22,6 +25,15 @@ function startGame() {
   <div class="HomePage">
     <NavBar title="MTG Treachery" no-back-button />
     <MenuButton text="Start a Game" @click="startGame" />
+    <MenuButton
+      text="Current Game"
+      v-if="homePageResult?.viewer?.currentGame"
+      @click="
+        router.push({
+          path: `/game/${homePageResult?.viewer?.currentGame?.id}`,
+        })
+      "
+    />
     <MenuButton text="Logout" @click="logout" />
   </div>
 </template>
